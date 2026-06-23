da88f950497e42b7d444fe5350596c780a53427d
oleg-afonick <oleg.afonick@gmail.com>
2026-06-23 22:51:23 +0500

    Добавили Open API документацию

--- sprints/urls.py
+++ sprints/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 from rest_framework.routers import DefaultRouter
 
 from pereval.views import PerevalViewSet
+from sprints.yasg import urlpatterns as yasg_urls
 
 router = DefaultRouter()
 
@@ -11,5 +13,8 @@ router.register(r'pereval', PerevalViewSet, basename='pereval')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += yasg_urls
